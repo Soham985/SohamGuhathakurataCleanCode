2e98bd38ca2dddc8481d51d3a30298f4d5d48bbe
Soham958 <sohamguhathakurata1999@gmail.com>
2020-02-16 16:29:32 +0530

    commit

---
+++ CleanCode/src/main/java/com/CleanCode/maven/CleanCode/App.java
@@ -0,0 +1,63 @@
+package com.CleanCode.maven.CleanCode;
+import java.util.*;
+class InterestCalculator
+{
+	static double SimpleInterest(double principle, double year, double rate)
+	{
+		double SI=(principle*year*rate)/100;
+		return SI;
+	}
+	static double CompoundInterest(double principle,double year, double rate)
+	{
+		double increment=1+(rate/100);
+		double CI=Math.pow(increment, year)*principle;
+		return CI;
+	}
+}
+
+class ConstructionCost
+{
+	static double TotalCost(int materialChoice,double area)
+	{
+		double total_cost=0;
+		switch(materialChoice)
+		{
+		case 1:
+			total_cost=1200*area;
+			break;
+		case 2:
+			total_cost=1500*area;
+			break;
+		case 3:
+			total_cost=1800*area;
+			break;
+		case 4:
+			total_cost=2500*area;
+			break;
+		}
+		return total_cost;
+	}
+		
+}
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Scanner sc=new Scanner(System.in);
+    	System.out.append("Enter the princile amount, year, and rate of interest");
+    	double principle=sc.nextDouble();
+    	double year=sc.nextDouble();
+    	double rate=sc.nextDouble();
+    	System.out.append("Press 1 to check Simple Interest\nPress 2 to check Compound Interest");
+    	int choice=sc.nextInt();
+    	if(choice==1)
+    		InterestCalculator.SimpleInterest(principle,year,rate);
+    	else
+    		InterestCalculator.CompoundInterest(principle,year,rate);
+    	System.out.append("Enter yout housing area:");
+    	float area=sc.nextFloat();
+    	System.out.append("Press 1 for standard material\nPress 2 for above standard material\nPress 3 for high standard material\nPress 4 for high standard material and fully automated home");
+    	choice=sc.nextInt();
+    	ConstructionCost.TotalCost(choice,area);
+    }
+}
